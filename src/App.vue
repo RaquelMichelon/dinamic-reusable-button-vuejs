3decc6110ae465b3372f94b7e95bd668b0cb5be6
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2023-02-24 14:02:48 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <app-button @click="handleClickShowAlert" buttonText="Click Me" />
+    <app-button
+      @click="handleClickPrintConsole"
+      :dark="true"
+      buttonText="Click Me"
+    />
+  </div>
+</template>
+
+<script>
+import AppButton from "./components/Button";
+export default {
+  name: "App",
+  components: {
+    AppButton,
+  },
+  methods: {
+    handleClickShowAlert() {
+      alert("Hi from button clicked!");
+    },
+    handleClickPrintConsole() {
+      console.log("Hi from button clicked!");
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button v-bind="$attrs" :class="[dark ? 'dark' : 'light', 'baseButton']">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      default: () => "Button Text",
+    },
+    dark: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.baseButton {
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.light {
+  background: white;
+  color: black;
+  border: 1px solid gray;
+}
+.dark {
+  background: black;
+  color: white;
+}
+</style>
